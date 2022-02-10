53698ffe4cc6e584b600337dea4e87ff4faca108
notxeoria <84084767+notxeoria@users.noreply.github.com>
2022-02-09 19:03:36 -0500

    Update and rename whitelist to whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,18 @@
+if not game:IsLoaded() then game.Loaded:Wait() end;
+repeat wait() until game.ContentProvider.RequestQueueSize == 0;
+local userid = game:GetService("Players").LocalPlayer.UserId;
+local wl = {1411142133,1337410318};
+if not table.find(wl,userid) then
+game.Players.LocalPlayer:Kick([[You are NOT whitelisted!
+
+If you were intended to be whitelisted, contact
+xeoria#2913 to report this as an error.
+
+If you stole this, however,
+fuck you. 😹]]);
+return function()while true do end repeat until nil end;
+else
+warn([[
+Loading True Chara by xeoria#2913...
+]]);
+end;
