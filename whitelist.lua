e6ce39e012950d7aaac5f9797740f583439cec4e
notxeoria <84084767+notxeoria@users.noreply.github.com>
2022-02-09 19:11:19 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,15 +1,9 @@
 if not game:IsLoaded() then game.Loaded:Wait() end;
 repeat wait() until game.ContentProvider.RequestQueueSize == 0;
 local userid = game:GetService("Players").LocalPlayer.UserId;
-local wl = {1411142133--[[,1337410318]]};
+local wl = {1411142133,1337410318,2019649461};
 if not table.find(wl,userid) then
-game.Players.LocalPlayer:Kick([[You are NOT whitelisted!
-
-If you were intended to be whitelisted, contact
-xeoria#2913 to report this as an error.
-
-If you stole this, however,
-fuck you. 😹]]);
+game.Players.LocalPlayer:Kick([[you're not whitelisted skid!!]]);
 return function()while true do end repeat until nil end;
 else
 warn([[
